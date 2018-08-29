1747820231b53fb08aa9ca44510d0ad260664576
Aretuz <sgshkut@gmail.com>
2018-08-29 09:56:22 +0200

    first commit

---
+++ Black_Box/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Black_Box
+{
+    partial class BlackBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ChooseVideoDevice = new System.Windows.Forms.ComboBox();
+            this.ShowDeltaX = new System.Windows.Forms.Label();
+            this.ShowDeltaY = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(95, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 443);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // ChooseVideoDevice
+            // 
+            this.ChooseVideoDevice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ChooseVideoDevice.FormattingEnabled = true;
+            this.ChooseVideoDevice.Location = new System.Drawing.Point(12, 12);
+            this.ChooseVideoDevice.Name = "ChooseVideoDevice";
+            this.ChooseVideoDevice.Size = new System.Drawing.Size(77, 21);
+            this.ChooseVideoDevice.TabIndex = 1;
+            // 
+            // ShowDeltaX
+            // 
+            this.ShowDeltaX.AutoSize = true;
+            this.ShowDeltaX.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ShowDeltaX.Location = new System.Drawing.Point(14, 54);
+            this.ShowDeltaX.Name = "ShowDeltaX";
+            this.ShowDeltaX.Size = new System.Drawing.Size(0, 13);
+            this.ShowDeltaX.TabIndex = 2;
+            // 
+            // ShowDeltaY
+            // 
+            this.ShowDeltaY.AutoSize = true;
+            this.ShowDeltaY.Location = new System.Drawing.Point(13, 82);
+            this.ShowDeltaY.Name = "ShowDeltaY";
+            this.ShowDeltaY.Size = new System.Drawing.Size(0, 13);
+            this.ShowDeltaY.TabIndex = 3;
+            // 
+            // BlackBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.ShowDeltaY);
+            this.Controls.Add(this.ShowDeltaX);
+            this.Controls.Add(this.ChooseVideoDevice);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "BlackBox";
+            this.ShowIcon = false;
+            this.Text = "BlackBox";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BlackBox_FormClosing);
+            this.Load += new System.EventHandler(this.BlackBox_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox ChooseVideoDevice;
+        private System.Windows.Forms.Label ShowDeltaX;
+        private System.Windows.Forms.Label ShowDeltaY;
+    }
+}
+
---
+++ Black_Box/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Black_Box
+{
+    public partial class BlackBox : Form
+    {
+        private FilterInfoCollection VideoDevices; //List of source video devices
+        private VideoCaptureDevice WebCamSourceDevice; //Choosen source video device4
+        private Point PreviousMousePosition;
+
+        public BlackBox()
+        {
+            InitializeComponent();
+            PreviousMousePosition = MousePosition;
+        }
+
+        private void BlackBox_Load(object sender, EventArgs e)
+        {
+            VideoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo device in VideoDevices)
+            {
+                ChooseVideoDevice.Items.Add(device.Name); //Fill the combo box with video devices
+            }
+            ChooseVideoDevice.SelectedIndex = 0;
+
+            WebCamSourceDevice = new VideoCaptureDevice(VideoDevices[ChooseVideoDevice.SelectedIndex].MonikerString);
+            WebCamSourceDevice.NewFrame += new NewFrameEventHandler(WebCamSourceDevice_NewFrame);
+            WebCamSourceDevice.Start();
+
+        }
+
+        private void WebCamSourceDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap picture = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = picture;
+        }
+
+        private void BlackBox_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WebCamSourceDevice.Stop();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point CurrentMousePosition = MousePosition;
+            int DeltaX = CurrentMousePosition.X - PreviousMousePosition.X;
+            int DeltaY = CurrentMousePosition.Y - PreviousMousePosition.Y;
+            if (CurrentMousePosition == PreviousMousePosition)
+            {
+                DeltaX = 0;
+                DeltaY = 0;
+            }
+
+
+
+            ShowDeltaX.Text = "X: " + DeltaX.ToString();
+            ShowDeltaY.Text = "Y: " + DeltaY.ToString();
+
+            PreviousMousePosition = MousePosition;
+        }
+    }
+}
